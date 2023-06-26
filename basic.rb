10540ba99c5d2696b74b53013e6fc404101e87c3
Buendia <kenneth.buendia.code@gmail.com>
2023-06-26 18:07:18 +0900

    my first commit

---
+++ basic.rb
@@ -0,0 +1,15 @@
+#2022/10/11-17:41
+
+list = {first:"javascript", second:"php", third:"python"}
+
+puts list
+
+#2023/06/25/14:
+class Main 
+    attr_accessor :var
+
+    def initialize()
+        @var 
+
+    end
+end
